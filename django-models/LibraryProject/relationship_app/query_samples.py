0334b3f9eb57a44c135eea29e28245bb41496ed3
Kalkidan-dev <cakek433@gmail.com>
2025-07-18 14:34:05 +0300

    Some Update

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,29 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
-author = Author.objects.get(name="J.K. Rowling")
-books_by_author = Book.objects.filter(author=author)
+# 1. Query all books by a specific author
+try:
+    author = Author.objects.get(name="J.K. Rowling")
+    books_by_author = Book.objects.filter(author=author)
+except Author.DoesNotExist:
+    books_by_author = []
+    print("Author not found.")
 
-# List all books in a library
-library = Library.objects.get(name="Central Library")
-books_in_library = library.books.all()
+# 2. List all books in a library
+try:
+    library = Library.objects.get(name="Central Library")
+    books_in_library = library.books.all()
+except Library.DoesNotExist:
+    books_in_library = []
+    print("Library not found.")
 
-# Retrieve the librarian for a library
-librarian = Librarian.objects.get(library=library)
+# 3. Retrieve the librarian for a library
+try:
+    librarian = library.librarian  # Uses OneToOneField reverse access
+    librarian_name = librarian.name
+except (Library.DoesNotExist, Librarian.DoesNotExist, AttributeError):
+    librarian_name = "Librarian not found."
 
+# Output the results
 print("Books by author:", [book.title for book in books_by_author])
 print("Books in library:", [book.title for book in books_in_library])
-print("Librarian name:", librarian.name)
+print("Librarian name:", librarian_name)
