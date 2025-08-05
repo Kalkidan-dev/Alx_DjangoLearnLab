1c6d12d06e99ff67d5e87fb4c10ce1198e5814b4
Kalkidan-dev <cakek433@gmail.com>
2025-08-05 16:38:07 +0300

    Edited

--- advanced_features_and_security/LibraryProject/relationship_app/urls.py
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views  
 from .views import list_books, LibraryDetailView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -9,13 +11,10 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('admin-only/', views.admin_view, name='admin_view'),
     path('librarian-only/', views.librarian_view, name='librarian_view'),
-    path('library/<int:pk>/fb/', views.library_detail_function, name='library_detail_function'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'), 
     path('member-only/', views.member_view, name='member_view'),
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
-    path('books/', views.list_books, name='list_books'),
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
