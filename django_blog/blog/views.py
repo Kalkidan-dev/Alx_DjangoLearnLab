5690db5356b6d468e48e9deecc70029141708ad0
Kalkidan-dev <cakek433@gmail.com>
2025-08-14 16:22:13 +0300

    Created

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import UserRegisterForm, UserUpdateForm
 
-# Create your views here.
+# Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile View
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'blog/profile.html', {'u_form': u_form})
+
+# Login and Logout using Django's built-in views
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
