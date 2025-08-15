9d4c657c4db3134e8b93020e1a0dd07192142477
Kalkidan-dev <cakek433@gmail.com>
2025-08-15 10:47:12 +0300

    Modifed

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -13,7 +13,20 @@ from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Post, Comment
 from .forms import CommentForm
 from django.db.models import Q
+from taggit.models import Tag
 
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['tag_slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = Tag.objects.get(slug=self.kwargs['tag_slug'])
+        return context
 def posts_by_tag(request, tag_name):
     posts = Post.objects.filter(tags__name__iexact=tag_name)
     return render(request, 'blog/post_list.html', {'posts': posts, 'tag_name': tag_name})
