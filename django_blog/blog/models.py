f0b245872f376067f68d5c0466d9d9dace641816
Kalkidan-dev <cakek433@gmail.com>
2025-08-15 10:31:55 +0300

    Modifed

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
+from taggit.managers import TaggableManager  # import this
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -15,14 +10,14 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
-
+    tags = TaggableManager(blank=True)  # <-- use django-taggit
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
