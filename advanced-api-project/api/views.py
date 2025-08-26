8fbe91b9ce19f58ef919e77ae2c1935cce99ed31
Kalkidan-dev <cakek433@gmail.com>
2025-08-26 11:51:30 +0300

    Correct the import and add the missing import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,5 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import generics, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
@@ -11,17 +10,17 @@ class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    # Use filters namespace
     filter_backends = [
-        rest_framework.DjangoFilterBackend,  
-        SearchFilter,
-        OrderingFilter,
+        rest_framework.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,  
     ]
 
     filterset_fields = ["title", "author", "publication_year"]
     search_fields = ["title", "author__name"]
     ordering_fields = ["title", "publication_year"]
     ordering = ["title"]
-
 
 class BookDetailView(generics.RetrieveAPIView):
     """
