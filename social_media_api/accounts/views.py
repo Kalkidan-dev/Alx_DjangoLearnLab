7e8919c48425b03640d2ee614ac562c30862b5fe
Kalkidan-dev <cakek433@gmail.com>
2025-08-27 13:40:42 +0300

    add a view for follow and unfollow

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from rest_framework.decorators import api_view, permission_classes
+
 
 User = get_user_model()
 
@@ -39,3 +41,30 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        target_user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    if target_user == request.user:
+        return Response({"detail": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+
+    request.user.following.add(target_user)
+    return Response({"detail": f"You are now following {target_user.username}."}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        target_user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    request.user.following.remove(target_user)
+    return Response({"detail": f"You have unfollowed {target_user.username}."}, status=status.HTTP_200_OK)
